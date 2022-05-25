f94acf459da53f33d39c198049983fc91e620fef
MuhammadAbdullah72 <106270096+MuhammadAbdullah72@users.noreply.github.com>
2022-05-25 10:59:13 -0700

    Add files via upload

---
+++ Arrayporgram.java
@@ -0,0 +1,9 @@
+ublic class Arrayporgram
+{
+  public static void main(String args[])
+   {
+     String names[] = {"Abdullah", "Huzaifa", "Sheraaz", "ShereAli"};
+      System.out.println(names.length);
+   }
+}
+ 
