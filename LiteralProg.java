75306a36301dda3a6b1841aa45d1015487e30cd3
MuhammadAbdullah72 <106270096+MuhammadAbdullah72@users.noreply.github.com>
2022-05-25 10:47:49 -0700

    Add files via upload

---
+++ LiteralProg.java
@@ -0,0 +1,36 @@
+public class LiteralProg
+{
+public static void main(String args[])
+
+//BOOLEAN TYPE LITERALS
+
+boolean flag1 = false;
+boolean flag2 = true;
+
+// binary
+int binaryNumber = 0b10110;
+// octal 
+int octalNumber = 023;
+
+// decimal
+int decNumber = 36;
+
+// hexadecimal 
+int hexNumber = 0x3F; // 0x represents hexadecimal
+// binary
+int binNumber = 0b10010; // 0b represents binary
+
+//Float And Double
+
+double myDouble = 3.4;
+    float myFloat = 3.4F;
+ 
+    // 3.445*10^2
+    double myDoublepower = 3.445e2;
+    
+    char letter = 'h';
+    
+    String str1 = "Java Programming language ";
+    String str2 = "My name Is HAsnain ";
+
+}
